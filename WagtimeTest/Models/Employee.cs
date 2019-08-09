3ed02391f56eac64af36d8d172f1feaee4c45297
sebudwell@gmail.com <sebudwell@gmail.com>
2019-08-09 11:24:09 -0500

    Added Username field/employee model

---
+++ WagtimeTest/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WagtimeTest.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+
+        //public IList<Review> Reviews { get; set; }
+        //public IList<LocationCategory> LocationCategories { get; set; }
+    }
+}
